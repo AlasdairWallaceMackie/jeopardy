f31dfe8ca984895c3a3f6bca958d9ed6492e8c6a
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2022-09-06 17:29:42 -0400

    Create data file with placeholder information

---
+++ src/jeopardyData.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        name: "Category 1",
+        questions: [
+            {question: "Q10", answer: "A10"},
+            {question: "Q11", answer: "A11"},
+            {question: "Q12", answer: "A12"},
+            {question: "Q13", answer: "A13"},
+            {question: "Q14", answer: "A14"},
+        ],
+    },
+    {
+        name: "Category 2",
+        questions: [
+            {question: "Q20", answer: "A20"},
+            {question: "Q21", answer: "A21"},
+            {question: "Q22", answer: "A22"},
+            {question: "Q23", answer: "A23"},
+            {question: "Q24", answer: "A24"},
+        ],
+    },
+    {
+        name: "Category 3",
+        questions: [
+            {question: "Q30", answer: "A30"},
+            {question: "Q31", answer: "A31"},
+            {question: "Q32", answer: "A32"},
+            {question: "Q33", answer: "A33"},
+            {question: "Q34", answer: "A34"},
+        ],
+    },
+    {
+        name: "Category 4",
+        questions: [
+            {question: "Q40", answer: "A40"},
+            {question: "Q41", answer: "A41"},
+            {question: "Q42", answer: "A42"},
+            {question: "Q43", answer: "A43"},
+            {question: "Q44", answer: "A44"},
+        ],
+    },
+    {
+        name: "Category 5",
+        questions: [
+            {question: "Q50", answer: "A50"},
+            {question: "Q51", answer: "A51"},
+            {question: "Q52", answer: "A52"},
+            {question: "Q53", answer: "A53"},
+            {question: "Q54", answer: "A54"},
+        ],
+    },
+    {
+        name: "Category 6",
+        questions: [
+            {question: "Q60", answer: "A60"},
+            {question: "Q61", answer: "A61"},
+            {question: "Q62", answer: "A62"},
+            {question: "Q63", answer: "A63"},
+            {question: "Q64", answer: "A64"},
+        ],
+    },
+]
+
+export default data
